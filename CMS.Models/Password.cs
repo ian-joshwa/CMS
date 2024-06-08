2ea180d2e4e0dc0f6eeb3578674ce02a57af41c3
ian joshwa <ianjoshwa@gmail.com>
2024-06-08 03:38:02 -0700

    Profile Pic And Password Added

---
+++ CMS.Models/Password.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Models
+{
+    public class Password
+    {
+        public string CurrentPassword { get; set; }
+
+        public string NewPassword { get; set; }
+
+        [Compare("NewPassword", ErrorMessage = "Password Do not match")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
