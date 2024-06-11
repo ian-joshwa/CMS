4ecc71641cb9ff12a66d3af3f0872254b10ee7fb
ian joshwa <ianjoshwa@gmail.com>
2024-06-11 04:40:43 -0700

    New Changes

---
+++ CollegeManagementSystem/Areas/Student/Controllers/EnrollmentController.cs
@@ -0,0 +1,27 @@
+﻿using CMS.CommonHelper;
+using CMS.DataAccessLayer.Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollegeManagementSystem.Areas.Student.Controllers
+{
+    [Area("Student")]
+    public class EnrollmentController : Controller
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public EnrollmentController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public IActionResult Enrollment()
+        {
+
+            var sessions = _unitOfWork.SessionAppService.GetAll().Where(x => x.Status == SessionStatus.Pending).ToList();
+            Navigation.ActivePage = "/Student/Enrollment/Enrollment";
+            return View(sessions);
+        }
+    }
+}
